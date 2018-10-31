aba4d57ac2f4193c128e9cf415a3c3ce8b156daf
olexiychebotkevich <olexiychebotkevich@A34COMP11>
2018-10-31 16:15:19 +0200

    Add Autorization

---
+++ PhoneStore/UI/Autorization.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using UI.Classes;
+
+namespace UI
+{
+    /// <summary>
+    /// Interaction logic for Autorization.xaml
+    /// </summary>
+    public partial class Autorization : Window
+    {
+        public Autorization()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (UserName.Text != "" && UserMail.Text != "")
+            {
+                UserUI user = new UserUI { Name = UserName.Text, Mail = UserMail.Text };
+                MainWindow mw = new MainWindow();
+                mw.ShowDialog();
+                this.Close();
+            }
+        }
+    }
+}
