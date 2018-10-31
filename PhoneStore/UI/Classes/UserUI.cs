4d2b9d0402316ee95822a237d88793f3dca58ea1
olexiychebotkevich <olexiychebotkevich@A34COMP11>
2018-10-31 15:53:31 +0200

    Add class

---
+++ PhoneStore/UI/Classes/UserUI.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UI.Classes
+{
+   public class UserUI
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Mail { get; set; }
+        public List<OrderUI> orders { get; set; }
+    }
+}
